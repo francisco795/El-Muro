aa893fe6c5b4c3d913853860618507a75d137193
francisco795 <franciscohdzcruz9@gmail.com>
2022-06-16 13:36:37 -0500

    Documentation added

--- index.php
+++ index.php
@@ -3,28 +3,6 @@
 require 'admin/config.php';
 require 'functions.php';
 
-// Asi es nuestro codigo antes de hacer una funcion para conectarnos a la base de datos.
-
-// try {
-// 	$conexion = new PDO('mysql:host=localhost;dbname='.$bd_config['basedatos'], $bd_config['usuario'], $bd_config['pass']);
-// 	$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// // Prepared Statement
-// 	// 1.- Preparamos nuestra sentencia SQL
-// 	$statement = $conexion->prepare('SELECT * FROM articulos');
-
-// 	// 2.- Ejecutamos
-// 	$statement->execute();
-// 	$resultados = $statement->fetchAll();
-	
-
-// } catch (PDOException $e) {
-// 	echo 'ERROR:' . $e->getMessage();
-// 	// Si hay algun error podemos redirigir al usuario a una pagina de error
-
-// 	header('Location: error.php');
-// 	die('Problemas con la conexion');
-// }
 
 $conexion = conexion($bd_config);
 
@@ -32,11 +10,6 @@ if (!$conexion) {
 	header('Location: error.php');
 }
 
-# Este codigo es el que usaremos antes de programar la paginacion
-
-// $sentencia = $conexion->prepare("SELECT * FROM articulos LIMIT $post_por_pagina");
-// $sentencia->execute();
-// $posts = $sentencia->fetchAll();
 
 // Traemos los post dependiendo de cuantos por pagina y en que pagina nos encontremos
 
